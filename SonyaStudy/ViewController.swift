f06ee74fadc9925d12fed0b6c62f9b0e7cab8bd0
Danyl Timofeyev <timofeev.danil@gmail.com>
2021-10-11 16:37:57 +0300

    Initial Commit

---
+++ SonyaStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SonyaStudy
+//
+//  Created by Danyl Timofeyev on 11.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
